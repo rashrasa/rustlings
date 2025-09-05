98cef678b3028459328f93f1ac4d7ace812753fa
slenem <ahmed.rashrash@gmail.com>
2025-09-05 09:57:55 -0400

    progress 7

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,25 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut result: Vec<String> = vec![];
+
+        for element in input {
+            match element.1 {
+                Command::Uppercase => result.push(element.0.to_uppercase()),
+                Command::Trim => result.push(element.0.trim().to_string()),
+                Command::Append(n) => {
+                    let mut appended = element.0;
+                    for _ in 0..n {
+                        appended.push_str("bar")
+                    }
+                    result.push(appended)
+                }
+            }
+        }
+
+        result
+    }
 }
 
 fn main() {
@@ -37,8 +55,8 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
     use super::Command;
+    use super::my_module::transformer;
 
     #[test]
     fn it_works() {
